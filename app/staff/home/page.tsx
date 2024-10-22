503bc7787b54fb3af4a01178fdd251eb80bade03
wlfyzz <mail+a@thatdevwolfy.lol>
2024-10-22 22:54:27 +0100

    Update page.tsx

--- app/staff/home/page.tsx
+++ app/staff/home/page.tsx
@@ -69,11 +69,11 @@ export default function StaffHome() {
       try {
         const response = await fetch(`/api/stats?t=${new Date().toISOString()}`);
         const x: NowPlaying = await response.json();
-        setCurrentListeners(data.listeners.current);
-        setPeakListeners(Math.max(peakListeners, data.listeners.current));
+        setCurrentListeners(x.data.listeners.current);
+        setPeakListeners(Math.max(peakListeners, x.data.listeners.current));
         setCurrentTrack({
-          title: data.now_playing.song.title,
-          artist: data.now_playing.song.artist,
+          title: x.data.now_playing.song.title,
+          artist: x.data.now_playing.song.artist,
         });
       } catch (error) {
         console.error('Failed to fetch data:', error);
