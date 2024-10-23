69a50f1a281d269240794e1f23461356e56b5c2e
wlfyzz <mail+a@thatdevwolfy.lol>
2024-10-23 18:18:44 +0100

    Update page.tsx

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,36 +1,39 @@
-"use client"
-import { useEffect, useState } from 'react';
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Redirect() {
   const [countdown, setCountdown] = useState(2);
-  const allowedHosts: string | string[] = ["limeradio.net"]
+  const allowedHosts: string[] = ["limeradio.net"];
 
   useEffect(() => {
     const timer = setInterval(() => {
       setCountdown((prevCountdown) => prevCountdown - 1);
     }, 1000);
-    const redirectUrl = new URLSearchParams(window.location.search).get('r');
 
-    if (countdown === 2) {
+    const redirectUrl = new URLSearchParams(window.location.search).get("r");
+
+    if (countdown === 0) {
       clearInterval(timer);
-      if (redirectUrl?.split("https://")[1]) {
-        const host = redirectUrl?.split("https://")[1]
-        if (!allowedHosts.includes(host)) {return window.location.href = "/"}
-      }
 
       if (redirectUrl) {
-        window.location.href = redirectUrl;
+        const urlHost = new URL(redirectUrl).hostname;
+
+        if (!allowedHosts.includes(urlHost)) {
+          window.location.href = "/";
+        } else {
+          window.location.href = redirectUrl;
+        }
       } else {
-        window.location.href = '/';
+        window.location.href = "/";
       }
     }
 
     return () => clearInterval(timer);
-  }, [countdown]);
+  }, [countdown, allowedHosts]);
 
   return (
     <div
-      className={`flex flex-col items-center justify-center min-h-screen bg-lime-900 text-white`}
+      className="flex flex-col items-center justify-center min-h-screen bg-lime-900 text-white"
     >
       <h1 className="text-6xl font-bold mb-4">Redirecting...</h1>
       <p className="mb-4 text-center">You will be redirected in {countdown} seconds.</p>
