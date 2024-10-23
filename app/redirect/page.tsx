40a5ac8497016a045d7488d57d5c5bc023412afc
limeradio <mail+c@thatdevwolfy.lol>
2024-10-23 12:25:33 +0000

    limited the redirect hosts

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -3,15 +3,21 @@ import { useEffect, useState } from 'react';
 
 export default function Redirect() {
   const [countdown, setCountdown] = useState(2);
+  const allowedHosts: string | string[] = ["limeradio.net"]
 
   useEffect(() => {
     const timer = setInterval(() => {
       setCountdown((prevCountdown) => prevCountdown - 1);
     }, 1000);
+    const redirectUrl = new URLSearchParams(window.location.search).get('r');
 
-    if (countdown === 0) {
+    if (countdown === 2) {
       clearInterval(timer);
-      const redirectUrl = new URLSearchParams(window.location.search).get('r');
+      if (redirectUrl?.split("https://")[1]) {
+        const host = redirectUrl?.split("https://")[1]
+        if (!allowedHosts.includes(host)) {return window.location.href = "/"}
+      }
+
       if (redirectUrl) {
         window.location.href = redirectUrl;
       } else {
